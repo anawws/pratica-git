921c5f773057630a8d680cbaf9c87e0e3ffb4f2c
Ana Carolina <anacarolina0722@gmail.com>
2023-03-17 15:50:30 -0300

    Plotando múltiplas linhas no gráfico para múltiplos conjuntos de dados

--- Geragrafico.py
+++ Geragrafico.py
@@ -8,10 +8,14 @@ def main():
     valores = leitor.getValores()
     print(valores)
     
-    plt.plot(valores)
+  
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
     plt.title('Gráfico de linhas')
+
+    for serie in valores:
+        plt.plot(serie)
+
     plt.show()
 
 main() 
