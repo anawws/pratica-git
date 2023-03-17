55dbcab6200bd1bd196f1871eb1fbb37a2b92292
anacarolina <anacarolina0722@gmail.com>
2023-03-17 15:13:24 -0300

    Plotando gráfico de linha a partir dos dados do arquivo de entrada

--- Geragrafico.py
+++ Geragrafico.py
@@ -1,11 +1,14 @@
-from Leitorarquivo import LeitorArquivo
+import numpy as np
+import matplotlib.pyplot as plt
 
+from Leitorarquivo import LeitorArquivo
 
 def main():
     leitor = LeitorArquivo('data.txt')
     valores = leitor.getValores()
     print(valores)
-
+    
+    plt.plot(valores)
+    plt.show()
 
 main()
-
